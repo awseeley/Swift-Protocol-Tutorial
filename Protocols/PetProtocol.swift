195ec94ae1ef59b8137d7ca3283400a5ba15d11e
Andrew Seeley <awseeley@gmail.com>
2017-08-06 18:59:20 +1000

    Swift Protocol Tutorial
    Swift Protocol Tutorial

---
+++ Protocols/PetProtocol.swift
@@ -0,0 +1,22 @@
+//
+//  PetProtocol.swift
+//  Protocols
+//
+//  Created by Andrew Seeley on 6/8/17.
+//  Copyright © 2017 Seemu. All rights reserved.
+//
+
+import UIKit
+
+protocol Pet {
+    var name: String {get}
+    var age: Int {get set}
+    var legs: Int {get set}
+    
+    func makeSound()
+}
+
+protocol Clothing {
+    var sunglasses: String {get set}
+    var type: String {get set}
+}
